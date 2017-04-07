96a6a06bdc51b059464e6f8e2ff376c8700436cf
kianva <warmclue@gmail.com>
2017-04-07 18:04:23 +0800

    weibo crawler

--- multiple.py
+++ multiple.py
@@ -5,20 +5,20 @@ import threading #导入threading包
 from time import sleep  
 import time  
   
-def task1():   
-    print ("Task 1 executed." )  
+def task1(i,j):   
+    print ("Task 1 executed."+str(i)+':'+str(j) )  
     sleep(1)  
-    print('Task 1 over')
+   # print('Task 1 over')
   
 def task2():  
     print ("Task 2 executed." )  
     sleep(5)  
-    print('Task 2 over')
+    #print('Task 2 over')
       
 print("多线程：")  
 starttime=time.time(); #记录开始时间  
 threads = [] #创建一个线程列表，用于存放需要执行的子线程  
-t1 = threading.Thread(target=task1) #创建第一个子线程，子线程的任务是调用task1函数，注意函数名后不能有（）  
+t1 = threading.Thread(target=task1,args=(1,2)) #创建第一个子线程，子线程的任务是调用task1函数，注意函数名后不能有（）  
 threads.append(t1)#将这个子线程添加到线程列表中  
 t2 = threading.Thread(target=task2)#创建第二个子线程  
 threads.append(t2)#将这个子线程添加到线程列表中  
@@ -32,11 +32,11 @@ print ("耗时：{0:.5f}秒" .format(totaltime)); #格式输出耗时
 print('---------------------------')  
   
 #以下为普通的单线程执行过程，不需解释  
-print("单线程：")  
-starttime=time.time();  
-task1();  
-task2();  
-endtime=time.time();  
-totaltime=endtime-starttime;  
-print ("耗时：{0:.5f}秒" .format(totaltime));  
+# print("单线程：")  
+# starttime=time.time();  
+# task1('x');  
+# task2();  
+# endtime=time.time();  
+# totaltime=endtime-starttime;  
+# print ("耗时：{0:.5f}秒" .format(totaltime));  
 
